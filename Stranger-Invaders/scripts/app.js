b3184b2d1ba1071004ea970c84a5055d8d2dd4df
Riddles <ridwan.arshad@hotmail.com>
2022-07-19 13:33:31 +0100

    updated moveInvaders

--- Stranger-Invaders/scripts/app.js
+++ Stranger-Invaders/scripts/app.js
@@ -7,41 +7,40 @@ function init() {
   const startButton = document.querySelector('#start-button')
   const restartButton = document.querySelector('#restart-button')
   const score = document.querySelector('.score')
-  // const userName = document.querySelector('.username-btn')
   const demo = document.querySelector('.demo')
-  const shooter = document.getElementsByClassName('rShooter')
+  // const shooter = document.getElementsByClassName('rShooter')
+  const shooter = Array.from(document.getElementsByClassName('rShooter'))
   const elevenForce = document.getElementsByClassName('eForce')
   const hero = document.getElementsByClassName('hero')
   const buttons = document.querySelectorAll('.btns')
-  startButton.disabled = false
-  restartButton.disabled = true
+  console.log('demo', demo)
+ 
+  console.log('force', elevenForce)
+  console.log('hero', hero)
+  console.log('buttons', buttons)
 
-  // map out cells of the grid
+  
+
+  // map out cells of the grid onto an array
   const cells = Array.from(document.querySelectorAll('.grid div'))
+
+  console.log('shooter', shooter)
 
   // Disable restart at the beginning.
   restartButton.disabled = true
 
-  // ! Variables
   const width = 10
   const cellCount = width * width
   const topGrid = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
   const bottomGrid = [90, 91, 92, 93, 94, 95, 96, 97, 98, 99]
-  let cell = []
+  // let cell = []
   let invadersSpeed = 1500
   let currentScore = 0
   let currentLives = '3'
   // let timesPlay = 0
-  let height = width
-  const rightSide = grid[9, 19, 29, 39, 49, 59, 69, 79, 89, 99]
-  const leftSide = grid[0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
-
-
-  // const startingInvaders = [1, 2, 3, 4, 5, 6, 7, 8, 11, 12, 13, 14, 15, 16, 17, 
-  //   18, 21, 22, 23, 24, 25, 26, 27, 28, 31, 32, 33, 34, 35, 36, 37,38]
-
-
-
+  const rightSide = [9, 19, 29, 39, 49, 59, 69, 79, 89, 99]
+  const leftSide = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]
+  console.log('right', rightSide)
   // Character class
   // const charClass = 'hero'
   const startingPosition = 95
@@ -55,10 +54,16 @@ function init() {
     18, 21, 22, 23, 24, 25, 26, 27, 28, 31, 32, 33, 34, 35, 36, 37, 38]
   
   let strangeInvaders = startingInvaders
+  console.log('start', strangeInvaders)
+  //Events
+  document.addEventListener('keydown', heroMovement)
+  startButton.addEventListener('click', startGame)
+  restartButton.addEventListener('click', resetGame)
+  document.addEventListener('keydown', force)
 
   // Getting the high score
   function getHighScore() {
-    highScore = parseInt(localStorage.getItem('high-score')) || 0;
+    const highScore = parseInt(localStorage.getItem('high-score')) || 0
     console.log('highScore ->', highScore)
     high.innerHTML = (`${highScore}`)
   }
@@ -67,23 +72,22 @@ function init() {
   // Grid for display
   function createGrid() {
     for (let i = 0; i < cellCount; i++) {
-      let cell = document.createElement('div')
+      const cell = document.createElement('div')
       cell.dataset.index = i
       cells.push(cell)
       grid.appendChild(cell)
     }
     addHero(startingPosition)
     addForce(forcePosition)
+    addInvaders(startingInvaders)
   }
 
   // Add hero to position
   function addHero(currentPosition) {
-    const hero = document.getElementsByClassName('hero')
     cells[currentPosition].classList.add('hero')
   }
 
   function removeHero(currentPosition) {
-    const hero = document.getElementsByClassName('hero')
     cells[currentPosition].classList.remove('hero')
   }
 
@@ -119,49 +123,36 @@ function init() {
 
   // Add invaders to the cell at the beginning
   function addInvaders() {
-    // clearInterval()
-    // const startingInvaders = [1, 2, 3, 4, 5, 6, 7, 8, 11, 12, 13, 14, 15, 16, 17, 
-    //   18, 21, 22, 23, 24, 25, 26, 27, 28, 31, 32, 33, 34, 35, 36, 37,38]
     for (let i = 0; i < startingInvaders.length; i++)
       if (!invaderClass.includes(i)) {
         cells[startingInvaders[i]].classList.add('demo')
       }
   }
 
-  // // Similar to whack-a-mole game
-  // function removeInvaders(startingInvaders) {s
-  //   if (elevenForce === demo)
-  //     startingInvaders.classList.remove('demo')
-  //   elevenForce.classList.remove('elevenForce')
-  //   currentScore += 100
-  // }
 
-    // Add force to position
-    function addForce(forcePosition) {
-      // clearInterval(interval)
-      const elevenForce = document.getElementsByClassName('elevenForce')
-      cells[forcePosition].classList.add('elevenForce')
-    }
+  // Add force to position
+  function addForce(forcePosition) {
+    cells[forcePosition].classList.add('elevenForce')
+  }
   
 
-    function removeForce(forcePosition) {
-      const elevenForce = document.getElementsByClassName('elevenForce')
-      cells[forcePosition].classList.remove('elevenForce')
-    }
+  function removeForce(forcePosition) {
+    cells[forcePosition].classList.remove('elevenForce')
+  }
 
-    // function removeInvaders(startingInvaders) {
-    //   addForce()
-    //   if (elevenForce === demo)
-    //     startingInvaders.classList.remove('demo')
-    //   elevenForce.classList.remove('elevenForce')
-    //   currentScore += 100
-    //   removeForce()
-    // }
+  function removeInvaders(startingInvaders) {
+    addForce(forcePosition)
+    if (elevenForce === demo)
+      startingInvaders.classList.remove('demo')
+    elevenForce.classList.remove('elevenForce')
+    currentScore += 100
+    removeForce(topGrid)
+  }
 
   // Trying to add hero to the same cells as hero so that they move together.
   function force(event) {
     const keyCode = event.keyCode
-    const spaceUp = 32
+    const upMove = 32
     const left = 37
     const right = 39
     if (left === keyCode && forcePosition % width !== 0) {
@@ -172,66 +163,73 @@ function init() {
       removeForce(forcePosition)
       forcePosition += 1
       addForce(forcePosition)
-  if (spaceUp === keyCode && forcePosition) {
-    for (let i = 0; i < demo.length; i++){
-    if (elevenForce === demo)
-    startingInvaders.classList.remove('demo', 'elevenForce')
-    score += 100
+    } else if (upMove === keyCode && forcePosition >= width ) {
+      addForce(forcePosition)
+      forcePosition -= 1
+      // startingInvaders.classList.remove('demo')
+      currentScore += 100
+    }
   }
-}
-  }
-}
-
-function addShots(strangeInvaders) {
-  const shooter = document.getElementsByClassName('shooter')
-  cells[strangeInvaders].classList.add('shooter')
-  if (shooter){
-    return
-  }
-}
+  
 
 
-function removeShots(strangeInvaders) {
-  // const shooter = document.getElementsByClassName('.rShooter')
-  cells[strangeInvaders].classList.remove('shooter')
-}
+  function addShots() {
+    console.log('end', strangeInvaders)
+    shooter.forEach(rShooter => rShooter.classList.add('shooter'))
+    for (let i = 0; i < strangeInvaders.length; i++)
+      if (!strangeInvaders.includes(i)) {
+      // Array.from(shooter)
+        // console.log('strange', strangeInvaders)
+        cells[strangeInvaders].classList.add('shooter')
+        console.log('adding shots', addShots)
+      // console.log('shots', addShots)
+      }
+  }
+
+
+  function removeShots(strangeInvaders) {
+    cells[strangeInvaders].classList.remove('shooter')
+  }
   // for (let i = 0; i < startingInvaders.length; i++) {
   //   cell[startingInvaders[i]].classList.remove('demo')
 
   // Shots to fire from random length of the alienInvaders as this will cover the grid.
   function shots(){
     // shooter.classList.add('shooter')
-    const randomShots = startingInvaders[Math.floor(Math.random() * (startingInvaders.length - 2))]
-    addShots()
-    if (shots === hero){
+    addShots(strangeInvaders)
+    if (shots === 'hero'){
       currentLives -= 1
-      removeShots()
+      removeShots(strangeInvaders)
     } else {
       removeShots(bottomGrid)
     }
   }
 
-
   function moveInvaders(){
-    if (startingInvaders !== rightSide)
-     for (let i = 0; i < rightSide; i++) {
-       startingInvaders[i] = currentPosition += width
-       shots()
-     }
-     if (startingInvaders === rightSide)
-     for (let i = 0; i < leftSide; i++){
-       startingInvaders[i] = currentPosition -= width - 1
-       shots()
-     } else if (startingInvaders === hero){
-       window.alert(`Game Over`)
-     }
-    for (let i = 0; i < startingInvaders.length; i++)
-    if(!invaderClass.includes(i)) {
-     cells[startingInvaders[i]].classList.add('demo')
-    }
-  } 
-  
- 
+    if (strangeInvaders.some === rightSide) // checks to see if any are touching the right side. Using 'rightSide' as it helps me to visualise.
+      for (let i = 0; i < strangeInvaders.length; i++){ // Loops through all the strangerInvaders
+        strangeInvaders[i] += width - 1 // - 1 to move left all of them left
+        + 10 // to move down
+    } else if (strangeInvaders.some === leftSide)
+      for (let i = 0; i < strangeInvaders.length; i++) {
+        strangeInvaders[i] += width + 1 // plus one to move it all right
+        + 10 // to move down one row
+    } else if (startingInvaders.some === 'hero') // if some invaders touch the hero class it's over.
+      window.alert('Game Over')
+      console.log('moving', moveInvaders)
+  }
+
+
+  // (left === keyCode && currentPosition % width !== 0) {
+  //   removeHero(currentPosition)
+  //   currentPosition -= 1
+  //   addHero(currentPosition)
+  // } else if (right === keyCode && currentPosition % width !== width - 1) {
+  //   removeHero(currentPosition)
+  //   currentPosition += 1
+
+
+
 
   // function shooter() {
 
@@ -244,50 +242,44 @@ function removeShots(strangeInvaders) {
     addHero(startingPosition)
     addInvaders(startingInvaders)
     moveInvaders()
-    shots()
+    shots(strangeInvaders)
     startButton.disabled = true
     restartButton.disabled = false
     currentScore = 0
     currentLives = `${currentLives}`
-    // lives.innerHTML = (`${currentLives}`)
     score.innerHTML = (`${currentScore}`)
-    // createGrid() 
-    // addInvaders(startingInvaders)
     lives.innerHTML = '❤️'.repeat(currentLives)
-    interval = setInterval(moveInvaders, invadersSpeed)
-
-    // topScoreDisplay.innerHTML = url('./images/heart.gif').repeat(lives)
-    
-    // Need to add an if statement in here for if the invaders are hit then remove a life
+    interval
     if (shots === hero) {
       currentLives--
-      lives.innerHTML = lives ? '❤️'.repeat(lives) : "💔"
-      if (currentLives === 0) {
-        removeInvaders()
-        removeHero()
-        return endGame()
-      }
+      lives.innerHTML = lives ? '❤️'.repeat(lives) : '💔'
+    } else if (currentLives === 0) {
+      removeInvaders()
+      removeHero()
+      return endGame()
     }
   }
 
+  const interval = setInterval(moveInvaders, invadersSpeed)
+  
+
   function resetGame() {
     clearInterval(interval)
-    let lives = 3
-    let score = 0
+    // let lives = 3
+    // let score = 0
     invadersSpeed = 1500
-    // buttons.forEach(btn => btn.classList.remove('demo', 'hero'))
-    // removeHero()
+    removeHero()
     startGame()
   }
 
   function endGame() {
     clearInterval(interval)
-    if (currentScore > highScore) {
+    if (currentScore > high) {
       window.alert(`New High Score!\n ${currentScore}`)
       localStorage.setItem('highScore', currentScore)
       getHighScore()
     }
-    // startButton.disabled = false
+    startButton.disabled = false
     currentLives = 3
     currentScore = 0
     invadersSpeed = 1500
@@ -301,13 +293,11 @@ function removeShots(strangeInvaders) {
 
   createGrid()
 
-  document.addEventListener('keydown', heroMovement)
-  startButton.addEventListener('click', startGame)
-  restartButton.addEventListener('click', resetGame)
-  document.addEventListener('keydown', force)
+  
   // window.onload = function startGame() {}
 
 
-
 }
+
+
 window.addEventListener('DOMContentLoaded', init)
